4e60468a64feb5630a42253ac177a69b226480a4
Arjun Baidya <mr.arjunbaidya@gmail.com>
2023-05-15 16:24:19 +0530

    Creating the streamlit app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from transformers import BertTokenizer, BertForSequenceClassification
 
 # Getting the tokenizer and the model from Hugging Face hub where it's deployed
 
-@st.cache_data
+@st.cache_resource
 def get_model():
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     model = BertForSequenceClassification.from_pretrained("arjbaid/Fine_Tuned_BERT_Classifier")
@@ -21,7 +21,7 @@ st.write(f"## :blue[Toxic Comment Analyzer]")
 user_input = st.text_area("Enter the comment below for analysis:")
 button = st.button("Analyze")
 
-if user_input and button :
+if user_input and button:
     
     # Tokenizing the comment
 
@@ -43,4 +43,4 @@ if user_input and button :
 
     mapping = {0: "Non-Toxic", 1: "Toxic"}
     st.write(f":blue[Prediction:] Comment is _{mapping[y_pred[0]]}_ \
-             with a probability of _{probability:.4f}_")
+            with a probability of _{probability:.4f}_")
